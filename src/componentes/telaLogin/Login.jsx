9b35a867440a0b3997c4b390a80295efa42256dc
Gabriel Bispo <gab.bispo.gs@gmail.com>
2024-08-29 15:33:37 -0300

    updatelogin1_gab
    começando login.. primitivo. estudando react.

--- src/componentes/telaLogin/Login.jsx
+++ src/componentes/telaLogin/Login.jsx
@@ -1,6 +1,14 @@
-export function Login(){
+export function Login(props){
     return(
         <>
+        <form action="login_page" method="get">
+            <label for="login_email"> Login: </label>
+            <input type="email" name="login_email" id="login_email" placeholder="E-mail" />
+            <label for="login_senha"> Senha: </label>
+            <input type="senha" name="login_senha" id="login_senha" placeholder="Senha" />
+        </form>
+
+        <button type="submit" onClick={}></button>
         </>
     )
 }
