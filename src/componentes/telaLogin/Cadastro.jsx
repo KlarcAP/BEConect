5e4dbc246ba2ed7f7b8a37f942c9fd6fe7b2583f
makson kawan <makson.costa@ba.estudante.senai.ba>
2024-09-09 21:28:37 -0300

    Adicionando aqruivo  Cadastro

--- src/componentes/telaLogin/Cadastro.jsx
+++ src/componentes/telaLogin/Cadastro.jsx
@@ -1,6 +1,22 @@
-export function Cadastro(){
-    return(
-        <>
-        </>
+export function Cadastro() {
+    return (
+        <div id="Cad" className="tela_Cad">
+            <div className="container-info">
+                <h2>Novo cadastro</h2>
+                <input className="cad-email" type="emai" placeholder="E-mail"></input><br></br>
+                <input className="cad-user" type="text" placeholder="Usuário"></input><br></br>
+                <input className="cad-senha" type="password" placeholder="Senha"></input><br></br>
+
+                <button className="cad-buton" type="submit" onClick={() = {}}>Entrar</button><br></br>
+                <a href="https://accounts.google.com/signin"><button className="cad-buton" type="submit">Entrar com Google</button></a>
+
+
+            </div>
+            <div className="container-welcome">
+                <h1>SEJA</h1>
+                <h1>BEM - VINDO!</h1>
+                <a href="/Login" target="_self">Já possui conta?</a>
+            </div>
+        </div>
     )
 }
