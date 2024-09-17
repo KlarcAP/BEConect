786b6c04f391ad784dc7c3833afb337e1894fc8d
Gabriel Bispo <gab.bispo.gs@gmail.com>
2024-09-16 22:26:37 -0300

    att_front_login
    falta imagem etc mas vou ajeitar tudo

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,8 @@
 import './App.css';
 import { CardProject } from './componentes/CardProject';
 import { TelaHome } from './componentes/TelaHome';
-import { TelaLogin } from './componentes/TelaLogin';
+import TelaLogin  from './componentes/TelaLogin';
+// import { TelaLogin } from './componentes/TelaLogin'; ESSE NÃO FUNCIONA.. APESAR DE SER O CORRETO
 
 function App() {
   return (
