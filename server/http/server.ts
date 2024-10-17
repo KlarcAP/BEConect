3cd7591f2d7842baceaa6f85c8e44a73ca809a7f
unknown <klarcalmeida@gmail.com>
2024-10-17 16:23:49 -0300

    Contruindo a API com express

--- server/http/server.ts
+++ server/http/server.ts
@@ -1,15 +1,54 @@
-import fastify from 'fastify';
+require('dotenv').config();
+import express from 'express';
+import http from 'http';
+import bodyParser from 'body-parser';
+import cookieParser from 'cookie-parser';
+import compression from 'compression';
+import cors from 'cors';
+import mongoose from 'mongoose';
+import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken';
 
-const app = fastify()
 
-app.get('/api/', () => {
-    return "Rodando API"
+const app = express();
+
+app.use(cors({
+    credentials: true,
+}));
+
+app.use(bodyParser.json());
+app.use(cookieParser());
+app.use(compression());
+
+const server = http.createServer(app);
+
+server.listen(8080, () =>{
+    console.log('Server running on http://localhost:8080/')
+})
+app.get('/', (req, res) => {
+    res.status(200).json({ msg: 'Nem vindo a nossa API!'})
 })
 
-app.listen({port: 3333}).then(() => {
-    console.log('Server is running on port 3333')
+app.post('/auth/register', async(req, res) => {
+    const { username, email, password, confirmpassword } = req.body;
+
+    if (!username){
+        return res.status(422).json({ msg: 'Nome é obrigatório!' });
+    }
+
 })
 
-app.post('/createProjects', () => {
+const dbUser = process.env.DB_USER;
+const dbPassword = process.env.DB_PASS;
 
-})
+mongoose
+    .connect(
+        `mongodb+srv://${dbUser}:${dbPassword}@cluster0.8x3vu.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`
+    )
+    .then(() => {
+        app.listen(3333)
+        console.log("Conectou ao banco!")
+    })
+    .catch((err) => console.log(err))
+
+app.listen(3333)
--- src/components/TelaLogin/LoginPage.tsx
+++ src/components/TelaLogin/LoginPage.tsx
@@ -1,4 +1,3 @@
-import { setDefaultResultOrder } from 'dns';
 import './loginpage.scss';
 import { FaUser } from "react-icons/fa";
 import { FaLock } from "react-icons/fa";
@@ -30,51 +29,47 @@ export function LoginPage(){
   }
   return (
     <div className="logincontainer">
-        
       <div className="loginwelcome">
         <h2>BEconect</h2>
-        <img src="https://creazilla-store.fra1.digitaloceanspaces.com/icons/3195615/code-sandbox-icon-md.png" alt="CuboLogo" /> 
-        <div className='linksintro'>
-          <p>Novo por aqui?</p> {/*LINK1 A FAZER*/}
+        <img src="https://creazilla-store.fra1.digitaloceanspaces.com/icons/3195615/code-sandbox-icon-md.png" alt="CuboLogo" />
+        <div className="linksintro">
+          <p>Novo por aqui?</p>
           <a className="naopossuiconta" href="/">Cadastre-se</a>
         </div>
       </div>
 
-      <div className='loginforms'>
+      <div className="loginforms">
         <form onSubmit={handleSubmit}>
-          <h1 className='loginname'>Login</h1>
+          <h1 className="loginname">Login</h1>
 
           <div className="input-group">
-            <i className="fas fa-user"><FaUser/></i>
-            <input 
-              type="text" 
+            <i className="fas fa-user"><FaUser /></i>
+            <input
+              type="text"
               placeholder="USUÁRIO"
               value={user}
               onChange={(e) => setUser(e.target.value)}
             />
-
           </div>
 
           <div className="input-group">
             <i className="fas fa-lock"><FaLock /></i>
-            <input 
-              type="password" 
-              placeholder="SENHA" 
+            <input
+              type="password"
+              placeholder="SENHA"
               value={password}
               onChange={(e) => setPassword(e.target.value)}
             />
           </div>
 
-          <button type="submit" className="entrar" >Entrar</button>
+          <button type="submit" className="entrar">Entrar</button>
 
           <div className="or-text">ou</div>
-          
-            <button className="google">
-              Entrar com Google
-              <img className='GoogleImg' src="https://th.bing.com/th/id/R.0fa3fe04edf6c0202970f2088edea9e7?rik=joOK76LOMJlBPw&riu=http%3a%2f%2fpluspng.com%2fimg-png%2fgoogle-logo-png-open-2000.png&ehk=0PJJlqaIxYmJ9eOIp9mYVPA4KwkGo5Zob552JPltDMw%3d&risl=&pid=ImgRaw&r=0" alt="Google" />
-            </button>
-            
-          </div>
+
+          <button type='submit' className="google">
+            Entrar com Google
+            <img className="GoogleImg" src="https://th.bing.com/th/id/R.0fa3fe04edf6c0202970f2088edea9e7?rik=joOK76LOMJlBPw&riu=http%3a%2f%2fpluspng.com%2fimg-png%2fgoogle-logo-png-open-2000.png&ehk=0PJJlqaIxYmJ9eOIp9mYVPA4KwkGo5Zob552JPltDMw%3d&risl=&pid=ImgRaw&r=0" alt="Google" />
+          </button>
         </form>
       </div>
     </div>
